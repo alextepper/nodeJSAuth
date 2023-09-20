2cf5c32e55cf61b0b310700436713e6e5d94e3d6
alextepper <alextepper92@gmail.com>
2023-09-20 17:49:31 +0300

    port shange

--- app.js
+++ app.js
@@ -1,29 +1,36 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const authRoutes = require('./routes/authRoutes');
-const dbRoutes = require('./routes/dbRoutes');
-const cookieParser = require('cookie-parser');
-const { requireAuth, checkUser } = require('./middleware/authMiddleware');
+const express = require("express");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/authRoutes");
+const dbRoutes = require("./routes/dbRoutes");
+const cookieParser = require("cookie-parser");
+const { requireAuth, checkUser } = require("./middleware/authMiddleware");
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 // middleware
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(express.json());
 app.use(cookieParser());
 
 // view engine
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 // database connection
-const dbURI = 'mongodb+srv://alextepper92:eFkO0cj38XYpLZLU@nodetuts.5aduk05.mongodb.net/node-auth?retryWrites=true&w=majority';
+const dbURI =
+  "mongodb+srv://alextepper92:eFkO0cj38XYpLZLU@nodetuts.5aduk05.mongodb.net/node-auth?retryWrites=true&w=majority";
 
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
-  .then((result) => app.listen(3000))
+mongoose
+  .connect(dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then((result) => app.listen(PORT))
   .catch((err) => console.log(err));
 
 // routes
-app.get('*', checkUser);
-app.get('/smoothies', requireAuth, (req, res) => res.render('smoothies'));
-app.get('/profile', requireAuth, (req, res) => res.render('profile'));
+app.get("*", checkUser);
+app.get("/smoothies", requireAuth, (req, res) => res.render("smoothies"));
+app.get("/profile", requireAuth, (req, res) => res.render("profile"));
 app.use(authRoutes);
 app.use(dbRoutes);
